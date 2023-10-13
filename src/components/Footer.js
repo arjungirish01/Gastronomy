0d77852e9f855573000493f0210254dfa7ec0842
arjun girish <arjun@pixeldev.tech>
2023-10-13 22:14:27 +0200

    footer final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "../images/logo/logo.png";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Footer(){
   return(
-    <div className="footer mt-5 mb-3 mx-3" style={{'color':'white'}}>
+    <div className="footer container-fluid mt-5 mb-3" style={{'color':'white','backgroundColor':'rgb(67, 65, 65)'}}>
         <div className="row">
             <div className="col-4">
                 <div className="about">
-                    <div clasName="logo" style={{'display':'flex', 'alignItems':'center'}}>
+                    <div className="logo" style={{'display':'flex', 'alignItems':'center'}}>
                       <img className="img-fluid" src={logo} style={{'width':'50px','height':'50px'}} />
                       <h1 className="h1">PixelDev</h1>
                     </div>
@@ -15,7 +16,41 @@ export default function Footer(){
                     <p>Current version v1.0</p>
                 </div>
             </div>
+            <div className="col-4">
+                  <div className="contact">
+                    <h3 className="h3">Get in Touch</h3>
+                    <div className="d-flex align-items-center">
+                        <i className="bi bi-envelope-at-fill mx-2"></i>
+                        <p>support@pixeldev.tech</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                        <i className="bi bi-telephone mx-2"></i>
+                        <p>+49-17687884251</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                        <i className="bi bi-compass mx-2"></i>
+                        <p>Frankenstrasse, Nürnberg, Germany</p>
+                    </div>
+                  </div>
+              </div>
+              <div className="col-4">
+                  <div className="projects">
+                      <h3 className="h3">Developer</h3>
+                      <div className="d-flex align-items-center">
+                          <i class="bi bi-github mx-2"></i>
+                          <a href="#" target="_blank">GitHub</a>
+                      </div>
+                      <div className="d-flex align-items-center">
+                          <i class="bi bi-linkedin m-2"></i>
+                          <a href="#" target="_blank">LinkedIn</a>
+                      </div>
+                      <div className="d-flex align-items-center">
+                          <i class="bi bi-substack mx-2"></i>
+                          <a href="#" target="_blank">Portfolio</a>
+                      </div>
+                  </div>
+              </div>
         </div>
-    </div>
+      </div>
   )
 }
